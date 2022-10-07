17e9c87c1d5a2491d1843952c375c3562b476822
Jennifer Cristina <88689395+jennifer-cristina@users.noreply.github.com>
2022-10-07 16:58:05 -0300

    In progress: Construindo tela de gerenciamento dos jogos

--- src/screens/Games/index.js
+++ src/screens/Games/index.js
@@ -6,6 +6,10 @@ import { Image, ScrollView, Text, TouchableOpacity, View } from 'react-native';
 import styles from './style';
 import { MainHeader } from '../../components/Header/MainHeader';
 import feelings from '../../assets/images/feelings.gif';
+import clothes from '../../assets/images/clothes.gif';
+import brushingTeeth from '../../assets/images/brushingTeeth.gif';
+import friends from '../../assets/images/friends.png';
+import { COLORS } from '../../assets/const';
 
 export const Games = ({ navigation }) => {
 
@@ -22,39 +26,55 @@ export const Games = ({ navigation }) => {
                     selecione os jogos que estarão disponíveis para seu filho(a)
                 </Text>
 
-                <View style={styles.listGamesContainer}>
-                    <ScrollView style={styles.listGames}>
+                <ScrollView style={styles.listGames}>
 
-                        {/* <View style={styles.game}> */}
-                        <TouchableOpacity style={styles.buttonGame}>
-                            <Text style={styles.textGame}>SENTIMENTOS</Text>
-                            <Image source={feelings} />
-                        </TouchableOpacity>
-                        {/* </View> */}
+                    <View style={styles.listGamesContainer}>
 
-                        {/* <View style={styles.game}> */}
-                        <TouchableOpacity style={styles.buttonGame}>
-                            <Text style={styles.textGame}>SENTIMENTOS</Text>
-                            <Image source={feelings} />
-                        </TouchableOpacity>
-                        {/* </View> */}
+                        <View style={styles.game}>
+                            <TouchableOpacity style={styles.buttonGame}>
+                                <Text style={styles.textGame}>Sentimentos</Text>
+                                <View style={styles.imageGameContainer}>
+                                    <Image
+                                        source={feelings}
+                                        resizeMode='cover'
+                                        style={styles.imageGame} />
+                                </View>
+                            </TouchableOpacity>
+                        </View>
 
-                        {/* <View style={styles.game}> */}
-                        <TouchableOpacity style={styles.buttonGame}>
-                            <Text style={styles.textGame}>SENTIMENTOS</Text>
-                            <Image source={feelings} style={styles.imageGame} />
-                        </TouchableOpacity>
-                        {/* </View> */}
+                        <View style={styles.game}>
+                            <TouchableOpacity style={styles.buttonGame}>
+                                <Text style={styles.textGame}>Escolher roupas</Text>
+                                <View style={styles.imageGameContainer}>
+                                    <Image
+                                        source={clothes}
+                                        resizeMode='cover'
+                                        style={styles.imageGame} />
+                                </View>
+                            </TouchableOpacity>
+                        </View>
 
-                        {/* <View style={styles.game}> */}
-                        <TouchableOpacity style={styles.buttonGame}>
-                            <Text style={styles.textGame}>SENTIMENTOS</Text>
-                            <Image source={feelings} style={styles.imageGame} />
-                        </TouchableOpacity>
-                        {/* </View> */}
+                        <View style={styles.game}>
+                            <TouchableOpacity style={styles.buttonGame}>
+                                <Text style={styles.textGame}>Escovar os dentes</Text>
+                                <View style={styles.imageGameContainer}>
+                                    <Image
+                                        source={brushingTeeth}
+                                        resizeMode='cover'
+                                        style={styles.imageGame} />
+                                </View>
+                            </TouchableOpacity>
+                        </View>
 
-                    </ScrollView>
-                </View>
+                        <View style={styles.game}>
+                            <TouchableOpacity style={styles.buttonGame}>
+                                <Text style={styles.textGame}>Sentimentos</Text>
+                                <Image source={feelings} style={styles.imageGame} />
+                            </TouchableOpacity>
+                        </View>
+
+                    </View>
+                </ScrollView>
             </View>
 
         </View>
--- src/screens/Games/style.js
+++ src/screens/Games/style.js
@@ -1,5 +1,14 @@
 import { StatusBar, StyleSheet } from "react-native";
-import { COLORS } from "../../assets/const";
+import { shadow } from "react-native-paper";
+import { COLORS, FONTS } from "../../assets/const";
+
+const bottomShadow = {
+    shadowOffset: { width: 0, height: 0, },
+    shadowColor: 'black',
+    shadowOpacity: 0.5,
+    shadowRadius: 5,
+    elevation: 5,
+}
 
 const styles = StyleSheet.create({
     container: {
@@ -11,31 +20,61 @@ const styles = StyleSheet.create({
         flex: 15,
         alignItems: 'center',
         justifyContent: 'center',
+        alignSelf: 'stretch',
         backgroundColor: COLORS.white
     },
-    listGamesContainer: {
-        alignSelf: 'stretch',
-        // backgroundColor: COLORS.red
+    textSelectGame: {
+        // backgroundColor: COLORS.black
     },
     listGames: {
+        width: '100%',
         // backgroundColor: COLORS.blue
     },
+    listGamesContainer: {
+        width: '100%',
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 50,
+        // backgroundColor: COLORS.darkBlue
+    },
     game: {
-        // width: '100%',
-        // heighxt: '50%',
+        width: '70%',
+        height: '25%',
         margin: 10,
-        backgroundColor: COLORS.white
+        ...bottomShadow,
+        borderRadius: 40,
+        backgroundColor: COLORS.white,
     },
     buttonGame: {
-        // height: 150,
-        // width: '60%',
-        margin: 10,
-        backgroundColor: COLORS.white
+        height: 160,
+        width: '100%',
+        borderRadius: 40,
+        ...bottomShadow,
+        borderColor: COLORS.white,
+        backgroundColor: COLORS.white,
+    },
+    textGame: {
+        fontSize: 20,
+        fontFamily: FONTS.title,
+        color: COLORS.black,
+        zIndex: 2,
+        margin: 20,
 
     },
+    imageGameContainer: {
+        width: '100%',
+        height: 193,
+        borderRadius: 40,
+        backgroundColor: COLORS.white,
+        position: 'absolute',
+        overflow: 'hidden',
+    },
     imageGame: {
-        // width: '100%',
-        // height: '100%'
+        borderRadius: 40,
+        alignSelf: 'center',
+        width: '100%',
+        height: '100%',
     }
 
 });
