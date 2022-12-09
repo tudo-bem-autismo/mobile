6414f65a7c67005ba5230d7d5192885301e64ad8
Lukas Santos Venancio <88518524+LukasVenancio@users.noreply.github.com>
2022-12-09 00:59:43 -0300

    .

--- src/services/schedule.js
+++ src/services/schedule.js
@@ -22,7 +22,7 @@ export const taskRegisterService = async (data) => {
         }
 
     } catch (error) {
-        console.log(error)
+        //console.log(error)
         showErrorToast(error.response.data.message)
 
         return {
